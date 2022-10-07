fc2cf4a4e9936214bc5dca5cf08b65b478ff99a4
shindra david <shindradavid.dev@gmail.com>
2022-10-07 21:37:55 +0300

    Create initial seo meta tags

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,1 +1,25 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<svelte:head>
+	<title>Shindra David | Contact</title>
+	<meta name="description" content="" />
+	<link rel="canonical" href={$page.url.toString()} />
+	<!-- TWITTER -->
+	<meta name="twitter:site" content="" />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:description" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:image:alt" content="" />
+	<!-- OPEN GRAPH -->
+	<meta property="og:title" content="" />
+	<meta property="og:type" content="website" />
+	<meta property="og:image" content="" />
+	<meta property="og:image:alt" content="" />
+	<meta property="og:url" content={$page.url.toString()} />
+	<meta property="og:description" content="" />
+	<meta property="og:site_name" content="Wave DJ" />
+</svelte:head>
+
 <h1>Contact</h1>
--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -1,1 +1,25 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<svelte:head>
+	<title>Shindra David | Portfolio</title>
+	<meta name="description" content="" />
+	<link rel="canonical" href={$page.url.toString()} />
+	<!-- twitter -->
+	<meta name="twitter:site" content="" />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:description" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:image:alt" content="" />
+	<!-- open graph -->
+	<meta property="og:title" content="" />
+	<meta property="og:type" content="website" />
+	<meta property="og:image" content="" />
+	<meta property="og:image:alt" content="" />
+	<meta property="og:url" content={$page.url.toString()} />
+	<meta property="og:description" content="" />
+	<meta property="og:site_name" content="Wave DJ" />
+</svelte:head>
+
 <h1>Portfolio</h1>
