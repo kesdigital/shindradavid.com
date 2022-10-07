eb85f19b16293211dd556af3466740e7b89f1deb
shindra david <shindradavid.dev@gmail.com>
2022-10-07 21:07:02 +0300

    Remove types.d.ts and put types definition in app.d.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -7,3 +7,16 @@ declare namespace App {
 	// interface Error {}
 	// interface Platform {}
 }
+
+interface SeoMetadata {
+	title: string;
+	description: string;
+	twitter?: {
+		title: string;
+		description: string;
+		type: 'summary' | 'summary_large_image';
+		image: string;
+		imageAlt: string;
+	};
+	openGraph?: {};
+}
