1f400e21ef8742c7f00be5df519d4f3c2490663b
shindra david <shindradavid.dev@gmail.com>
2022-10-13 17:14:00 +0300

    Fix spacing on footer

--- src/lib/sections/footer/Footer.svelte
+++ src/lib/sections/footer/Footer.svelte
@@ -5,8 +5,15 @@
 
 <footer class="footer">
 	<p>Copyright &copy; {new Date().getFullYear()} - Shindra David</p>
-	<p>Built with <HeartFill /> and <SvelteIcon /> by yours truly.</p>
+	<p class="footer__credits">
+		Built with <span><HeartFill /></span> and <SvelteIcon /> by yours truly.
+	</p>
 </footer>
 
 <style lang="scss">
+	.footer {
+		text-align: center;
+		font-size: var(--fs-sm);
+		@include page-lr-padding;
+	}
 </style>
